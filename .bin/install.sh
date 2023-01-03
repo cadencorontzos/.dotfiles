40e5560bf789ddbe0cd49185ffffc011846e927f
cadencorontzos <cadencorontzos@gmail.com>
2023-01-03 14:33:46 -0800

    script typo

--- .bin/install.sh
+++ .bin/install.sh
@@ -6,7 +6,7 @@ if [ $? = 0 ]; then
 	echo "Checked out dotfiles. ";
 else
 	echo "Backing up pre-existing dotfiles.";
-	config checkout 2&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
