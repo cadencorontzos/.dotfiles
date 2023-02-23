ccc6382166f6da05cd0052c3241bfab697e475a1
cadencorontzos <60051669+cadencorontzos@users.noreply.github.com>
2023-02-23 02:24:24 +0000

    typos in script

--- .scripts/fonts.sh
+++ .scripts/fonts.sh
@@ -1,12 +1,14 @@
 # Create the local fontconfig directory 
-$ mkdir -p ~/.config/fontconfig/conf.d/
+mkdir -p ~/.config/fontconfig/conf.d/
 
 # Create a file in that directory for the Nix fonts
-$ cat << EOF > ~/.config/fontconfig/conf.d/10-nix-fonts.conf
+cat << EOF > ~/.config/fontconfig/conf.d/10-nix-fonts.conf
 <?xml version='1.0'?>
 <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
 <fontconfig>
  <dir>~/.nix-profile/share/fonts/</dir>
 </fontconfig>
 EOF
+
+nix-env -iA nixpkgs.nerdfonts
 
