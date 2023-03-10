7ac4d1fef3b682354129f707400868447a6b67dd
cadencorontzos <60051669+cadencorontzos@users.noreply.github.com>
2023-03-09 22:40:06 -0800

    added js to coc nvim

--- vim/.config/nvim/lua/cadencorontzos/coc.lua
+++ vim/.config/nvim/lua/cadencorontzos/coc.lua
@@ -2,7 +2,7 @@
 vim.opt.backup = false
 vim.opt.writebackup = false
 
-vim.g.coc_global_extensions = {'coc-rust-analyzer', 'coc-clangd', 'coc-python', 'coc-lua'}
+vim.g.coc_global_extensions = {'coc-rust-analyzer', 'coc-clangd', 'coc-python', 'coc-lua', 'coc-tsserver'}
 -- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
 -- delays and poor user experience
 vim.opt.updatetime = 300
